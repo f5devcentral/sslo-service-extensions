f9728452f2cb1a827e89de3c3fbf6c0b4456b218
Kevin Stewart <kevin.g.stewart@gmail.com>
2025-02-12 13:24:02 -0500

    Update user-coaching-installer.sh

--- user-coaching/user-coaching-installer.sh
+++ user-coaching/user-coaching-installer.sh
@@ -14,7 +14,7 @@ echo "..Creating the iFile system object for the user-coaching-html"
 curl -sk \
 -u ${BIGUSER} \
 -H "Content-Type: application/json" \
--d '{"name": "user-coaching-html", "source-path": "https://raw.githubusercontent.com/kevingstewart/sslo-user-coaching/refs/heads/main/user-coaching-html"}' \
+-d '{"name": "user-coaching-html", "source-path": "https://raw.githubusercontent.com/f5devcentral/sslo-service-extensions/refs/heads/main/user-coaching/user-coaching-html"}' \
 https://localhost/mgmt/tm/sys/file/ifile/ -o /dev/null
 
 # ## Create iFile LTM object (user-coaching-html)
@@ -30,7 +30,7 @@ echo "..Creating the iFile system object for the user-blocking-html"
 curl -sk \
 -u ${BIGUSER} \
 -H "Content-Type: application/json" \
--d '{"name": "user-blocking-html", "source-path": "https://raw.githubusercontent.com/kevingstewart/sslo-user-coaching/refs/heads/main/user-blocking-html"}' \
+-d '{"name": "user-blocking-html", "source-path": "https://raw.githubusercontent.com/f5devcentral/sslo-service-extensions/refs/heads/main/user-coaching/user-blocking-html"}' \
 https://localhost/mgmt/tm/sys/file/ifile/ -o /dev/null
 
 # ## Create iFile LTM object (user-blocking-html)
@@ -43,7 +43,7 @@ https://localhost/mgmt/tm/ltm/ifile -o /dev/null
 
 # ## Install user-coaching-rule iRule
 echo "..Creating the user-coaching-rule iRule"
-rule=$(curl -sk https://raw.githubusercontent.com/kevingstewart/sslo-user-coaching/refs/heads/main/user-coaching-rule | awk '{printf "%s\\n", $0}' | sed -e 's/\"/\\"/g;s/\x27/\\'"'"'/g')
+rule=$(curl -sk https://raw.githubusercontent.com/f5devcentral/sslo-service-extensions/refs/heads/main/user-coaching/user-coaching-rule | awk '{printf "%s\\n", $0}' | sed -e 's/\"/\\"/g;s/\x27/\\'"'"'/g')
 data="{\"name\":\"user-coaching-rule\",\"apiAnonymous\":\"${rule}\"}"
 curl -sk \
 -u ${BIGUSER} \
@@ -53,7 +53,7 @@ https://localhost/mgmt/tm/ltm/rule -o /dev/null
 
 # ## Install user-coaching-ja4t-rule iRule
 echo "..Creating the user-coaching-ja4t-rule iRule"
-rule=$(curl -sk https://raw.githubusercontent.com/kevingstewart/sslo-user-coaching/refs/heads/main/user-coaching-ja4t-rule | awk '{printf "%s\\n", $0}' | sed -e 's/\"/\\"/g;s/\x27/\\'"'"'/g')
+rule=$(curl -sk https://raw.githubusercontent.com/f5devcentral/sslo-service-extensions/refs/heads/main/user-coaching/user-coaching-ja4-rule | awk '{printf "%s\\n", $0}' | sed -e 's/\"/\\"/g;s/\x27/\\'"'"'/g')
 data="{\"name\":\"user-coaching-ja4t-rule\",\"apiAnonymous\":\"${rule}\"}"
 curl -sk \
 -u ${BIGUSER} \
@@ -66,7 +66,7 @@ echo "..Creating the SSLO user-coaching inspection service"
 curl -sk \
 -u ${BIGUSER} \
 -H "Content-Type: application/json" \
--d "$(curl -sk https://raw.githubusercontent.com/kevingstewart/sslo-user-coaching/refs/heads/main/user-coaching-service)" \
+-d "$(curl -sk https://raw.githubusercontent.com/f5devcentral/sslo-service-extensions/refs/heads/main/user-coaching/user-coaching-service)" \
 https://localhost/mgmt/shared/iapp/blocks -o /dev/null
 
 ## Sleep for 15 seconds to allow SSLO inspection service creation to finish
